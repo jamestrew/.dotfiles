1b4c9401e1747d3f52232c7e98c404afef652853
James Trew <j.trew10@gmail.com>
2025-01-31 20:49:01 -0500

    nix tweaks

--- .config/qtile/scripts/startup.sh
+++ .config/qtile/scripts/startup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-# xrandr \
-#   --output HDMI-A-0 --mode 1920x1080 --pos 760x0 --rotate normal \
-#   --output DisplayPort-1 --mode 3440x1440 --pos 0x1080 --rotate normal \
-#   --output DisplayPort-0 --off \
-#   --output DisplayPort-2 --off
+
+xrandr \
+  --output DP-2 --mode 3440x1440 --pos 0x1080 --rotate normal \
+  --output HDMI-1 --mode 1920x1080 --pos 760x0 --rotate normal \
+  --output DP-1 --off \
+  --output DP-3 --off
+
 picom --experimental-backends &
 # clipmenud &
 # nitrogen --restore &
