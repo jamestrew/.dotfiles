fe8b09abcf51933e6fd1045459fda9b6a5c43587
James Trew <j.trew10@gmail.com>
2025-02-05 11:45:29 -0500

    move ta script under home-manager

--- home.nix
+++ home.nix
@@ -31,9 +31,15 @@
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    (pkgs.writeShellScriptBin "my-hello" ''
+      echo "Hello, ${config.home.username}!"
+    '')
+
+    (pkgs.writeShellApplication {
+      name = "ta";
+      runtimeInputs = [ pkgs.tmux pkgs.fd ];
+      text = builtins.readFile ./bin/ta;
+    })
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
